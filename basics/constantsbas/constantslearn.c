05d71cf3271f4b8cbd930035245f2d262508c03d
Lukas Frotton <132273525+Lukefrotts227@users.noreply.github.com>
2023-05-20 20:14:01 -0400

    Add files via upload

---
+++ basics/constantsbas/constantslearn.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define HEIGHT 12
+#define WIDTH 11
+#define LENGTH 12
+
+int main(){
+    int volume; 
+    volume = HEIGHT * WIDTH * LENGTH; 
+    printf("The area is...\n%i", volume);
+    return 0; 
+}
