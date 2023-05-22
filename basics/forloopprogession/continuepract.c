d01c23e3c75eab19e40372e25e823c5c71b975ed
Lukas Frotton <132273525+Lukefrotts227@users.noreply.github.com>
2023-05-22 13:16:48 -0400

    Add files via upload

---
+++ basics/forloopprogession/continuepract.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int i; 
+    int choice; 
+
+    for(i = 0; i < 5; i = i + 1){
+        printf("Enter 0 if you want to skip and anything else if you do not: ");
+
+        scanf("%i", &choice); 
+
+        if(choice == 0){
+            continue; 
+        }
+
+        printf("we printing this!!!\n");
+    }
+    return 0;
+}
