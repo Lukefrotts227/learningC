40f0afd00ad6e915f6355a64fe97a69c7ddd436b
Lukas Frotton <132273525+Lukefrotts227@users.noreply.github.com>
2023-05-23 15:47:47 -0400

    Add files via upload

---
+++ basics/scopebasic/scopeexm.c
@@ -0,0 +1,19 @@
+#include <stdio.h>; 
+
+
+int glob; 
+
+int main(){
+    int numfour = 4; 
+    int x; 
+
+    printf("Enter the number you want to add 4 too: ");
+
+    scanf("%i", &x); 
+
+    glob = x + numfour; 
+
+    printf("The number is %i", glob); 
+
+    return 0; 
+}
