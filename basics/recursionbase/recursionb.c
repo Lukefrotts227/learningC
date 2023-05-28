9d3bfd743886cfb255259337a1c37f9aecbab0c6
Lukas Frotton <132273525+Lukefrotts227@users.noreply.github.com>
2023-05-28 15:59:57 -0400

    Add files via upload

---
+++ basics/recursionbase/recursionb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int sum(int n); 
+
+int main(){
+    int num;
+    printf("Enter a number to apply the sum function to: "); 
+    scanf("%i", &num); 
+    int result = sum(num);
+    printf("The sum of all numbers is: %i", result);
+}
+
+
+int sum(int n){
+
+    if(n > 0){
+        return n + sum(n-1); 
+
+    }
+    return 0; 
+    
+}
