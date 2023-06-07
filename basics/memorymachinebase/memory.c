a21b53bbec9c6d1a74f2b550b37c3a01e06cfd8d
Ljf227 <ljfrotton@gmail.com>
2023-06-07 08:37:17 -0400

    my file memeory machine

---
+++ basics/memorymachinebase/memory.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// A basic "memory" file that writes information to a given file
+// includes the ability to clear that file
+// includes the ability to print out what is on the file
+// includes the ability to write new information to the file
+
+int main(){
+
+
+}
