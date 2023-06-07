f3063e44350d6ca994ed4268c0a84ec951625e4d
Ljf227 <ljfrotton@gmail.com>
2023-06-07 11:51:07 -0400

    continue the function

--- basics/tictaktoe/tiktactoe.c
+++ basics/tictaktoe/tiktactoe.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 
+char* getBoard(char arr[3][3]);
+
 int main(){ 
     printf("Welcome to the tictactoe game.\n"); 
     
@@ -28,15 +30,19 @@ int main(){
 
 
 
-char *getBoard(char arr[3][3]){
+char* getBoard(char arr[3][3]){
 
-    char* res = malloc(sizeof(arr)); 
+    char* res = malloc(sizeof(arr) + 6 ); 
 
-    
+    int ind = 0; 
     for (int i = 0; i < 3; i++){
 
         for(int j = 0; j < 3; j++){
-            
+            res[ind] = arr[i][j]; 
+            ind++; 
+
+            res[ind] = ' '; 
+
         }
 
     }
