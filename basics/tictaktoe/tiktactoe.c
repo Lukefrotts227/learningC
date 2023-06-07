17dc686b26b4b62d63cacae105da10440ab57555
Ljf227 <ljfrotton@gmail.com>
2023-06-07 11:45:16 -0400

    continuing the function

--- basics/tictaktoe/tiktactoe.c
+++ basics/tictaktoe/tiktactoe.c
@@ -30,6 +30,17 @@ int main(){
 
 char *getBoard(char arr[3][3]){
 
+    char* res = malloc(sizeof(arr)); 
+
+    
+    for (int i = 0; i < 3; i++){
+
+        for(int j = 0; j < 3; j++){
+            
+        }
+
+    }
+
 
 }
 
