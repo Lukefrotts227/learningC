b4916ab75acccace91f64fa886d7689f09f7abd0
Ljf227 <ljfrotton@gmail.com>
2023-06-07 11:38:55 -0400

    created the array for the game

--- basics/tictaktoe/tiktactoe.c
+++ basics/tictaktoe/tiktactoe.c
@@ -6,6 +6,13 @@
 int main(){ 
     printf("Welcome to the tictactoe game.\n"); 
     
+    char myArray[3][3] = {
+        {'n', 'n', 'n'},
+        {'n', 'n', 'n'},
+        {'n', 'n', 'n'}
+    }; 
+    
+
 
 
 
