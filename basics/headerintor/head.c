dc75d252e82b9e47410dd84089c595722ff89c6e
Ljf227 <ljfrotton@gmail.com>
2023-06-12 22:49:08 -0400

    the next part

--- basics/headerintor/head.c
+++ basics/headerintor/head.c
@@ -15,13 +15,20 @@ int main(){
         scanf("%i", &num1); 
         printf("Now enter the second one: "); 
         scanf("%i", &num2); 
-        tot = num1 + num2; 
+        tot = addtwo(num1,num2);  
 
         printf("The sum of them is: %i", tot); 
 
     }
 
     else if (choice == 1){
+        printf("Enter the first number: "); 
+        scanf("%i", &num1); 
+        printf("Enter the second one: "); 
+        scanf("%i", &num2); 
+
+        tot = multtwo(num1, num2);
+        printf("The two numbers multiplied is: %i", tot); 
 
     }
     else{
