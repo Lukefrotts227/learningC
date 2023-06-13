d382173b2ac33a7ce6c901e5d566fc5ecc7b4002
Ljf227 <ljfrotton@gmail.com>
2023-06-13 07:35:25 -0400

    building the structs

--- basics/librarymanage/librarybase.c
+++ basics/librarymanage/librarybase.c
@@ -1,8 +1,20 @@
-#include <stdio.h> 
+#include <stdio.h> // for the input output
+#include <stdbool.h> // to check if the book has been taken out or not
 
 
 
 // creating a libray management project in c
+
+// creating my structs
+
+struct library{
+
+};
+
+struct book{ 
+
+
+};
 
 int main(){
 
