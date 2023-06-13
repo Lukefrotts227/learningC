07d076b01be464792a7e8f6536dc0b2b85fa2687
Ljf227 <ljfrotton@gmail.com>
2023-06-12 22:43:44 -0400

    continue the work

--- basics/headerintor/head.c
+++ basics/headerintor/head.c
@@ -6,7 +6,19 @@ int main(){
     int num1; 
     int num2; 
 
-    
+    printf("Choose 1 to multiply and 0 to add: "); 
+    scanf("%i", &choice); 
+
+    if (choice == 0){
+
+    }
+
+    else if (choice == 1){
+
+    }
+    else{
+        printf("invalid choice!!!");
+    }
 
     
 
