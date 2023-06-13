4d7ffd48961da70754e15c598613a8b0e13c71eb
Ljf227 <ljfrotton@gmail.com>
2023-06-12 22:46:08 -0400

    the first part

--- basics/headerintor/head.c
+++ basics/headerintor/head.c
@@ -5,11 +5,19 @@ int main(){
     int choice; 
     int num1; 
     int num2; 
+    int tot; 
 
     printf("Choose 1 to multiply and 0 to add: "); 
     scanf("%i", &choice); 
 
     if (choice == 0){
+        printf("Enter the first number: "); 
+        scanf("%i", &num1); 
+        printf("Now enter the second one: "); 
+        scanf("%i", &num2); 
+        tot = num1 + num2; 
+
+        printf("The sum of them is: %i", tot); 
 
     }
 
