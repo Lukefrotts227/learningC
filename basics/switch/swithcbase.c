150c5af59c51a1cea71859d124c4922ddfd04b84
Ljf227 <ljfrotton@gmail.com>
2023-06-12 20:28:48 -0400

    Created a switch statement file

--- basics/switch/swithcbase.c
+++ basics/switch/swithcbase.c
@@ -1,10 +1,36 @@
 #include <stdio.h> 
 
-#include <stdlib.h> 
-#include <string.h> 
 
 
-
+// Grading system for test
 int main(){
+    int gpa_grade;
+    char letter_grade; 
+    printf("Enter the whole number gpa: "); 
+    scanf("%i", gpa_grade); 
+
+    switch(gpa_grade){
+        case 4: 
+        letter_grade = 'A'; 
+        break; 
+        case 3: 
+        letter_grade = 'B'; 
+        break; 
+        case 2: 
+        letter_grade = 'C'; 
+        break; 
+        case 1: 
+        letter_grade = 'D'; 
+        break; 
+        case 0: 
+        letter_grade = 'F'; 
+        break; 
+        
+    }
+
+    printf("The letter grade is: %c", letter_grade); 
+
+
+
 
 }
