d3af33a85fd72b8f9116b08a3c007f5a8f70d050
Ljf227 <ljfrotton@gmail.com>
2023-06-12 23:32:06 -0400

    it works

--- basics/macrosintro/macroin.c
+++ basics/macrosintro/macroin.c
@@ -7,7 +7,7 @@ int main(){
     float side_length; 
 
     printf("Enter the side length of the square: ");
-    scanf("%f", side_length); 
+    scanf("%f", &side_length); 
 
     float area = SquareArea(side_length); 
     printf("the area is %.2f", area); 
