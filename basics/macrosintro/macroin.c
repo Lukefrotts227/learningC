814d4bb6f4d3ff077ec5830f57e23cdef39271c4
Ljf227 <ljfrotton@gmail.com>
2023-06-12 23:29:51 -0400

    getting ready for debug

--- basics/macrosintro/macroin.c
+++ basics/macrosintro/macroin.c
@@ -4,6 +4,13 @@
 #define SquareArea(s) (s*s)
 
 int main(){
+    float side_length; 
+
+    printf("Enter the side length of the square: ");
+    scanf("%f", side_length); 
+
+    float area = SquareArea(side_length); 
+    printf("the area is %.2f", area); 
 
     return 0; 
 }
