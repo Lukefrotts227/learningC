3425402448aaa4d5f543387d7a240e734e333081
Ljf227 <ljfrotton@gmail.com>
2023-06-21 08:43:19 -0400

    beginning the linked list

--- datastructures/linkedlist/linekdlist.c
+++ datastructures/linkedlist/linekdlist.c
@@ -1,6 +1,27 @@
 #include <stdio.h> 
 
+typedef struct node {
+    int valud;
+    struct node *next;
+};
+
+void printLL(struct node *p)
+{
+    while (p != NULL)
+    {
+        printf("%d\n", p->valud); 
+        p = p->next; 
+    }
+}
+
+
 int main()
 {
+    struct node *head; 
+    struct node *one = NULL; 
+    struct node *two = NULL; 
+    struct node *three = NULL; 
+
+    
     return 0;
 }
