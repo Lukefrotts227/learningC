8b2c58814c36b19087443d6cd9cef353c4b1c298
Ljf227 <ljfrotton@gmail.com>
2023-06-21 09:04:04 -0400

    doubly linked list now

---
+++ datastructures/linkedlist/doubllylinkelist.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+typedef struct node
+{
+    int number; 
+    struct node *next; 
+    struct node *prev;
+}node;
+
+void printDLL(struct node *p)
+{
+
+    
+}
+
+
+int main() 
+{
+
+}
