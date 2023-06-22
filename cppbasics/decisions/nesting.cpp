6de763c5b78283ddd5339d066e2f7594fecebe44
Ljf227 <ljfrotton@gmail.com>
2023-06-22 08:02:37 -0400

    Finished the project for nesting loops

--- cppbasics/decisions/nesting.cpp
+++ cppbasics/decisions/nesting.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 
+using namespace std; 
 
 int main(void){
+    int num;
+    cout << "Enter a integer: "; 
+    cin >> num; 
 
-    
+    if (num != 0)
+    {
+        if (num > 0){
+            cout << "The number is positive." << endl; 
+
+        }
+
+        else {
+            cout << "The numbe is negative" << endl; 
+        }
+    }
+    else {
+        cout << "The number is 0" << endl; 
+    }
 
 
     // if it passes
