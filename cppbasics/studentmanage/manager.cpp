f82609db8e5b8a30f7da7ae33f4e4188ba70f2f0
Ljf227 <ljfrotton@gmail.com>
2023-06-22 11:53:39 -0400

    Starting the student manager with CPP

---
+++ cppbasics/studentmanage/manager.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+
+using namespace std;
+
+class Class {
+
+};
+
+class Student{
+
+};
+
+
+
+// the function to build the initial class
+void builder(){
+
+}
+
+// the main function
+int main(void){
+
+}
+
