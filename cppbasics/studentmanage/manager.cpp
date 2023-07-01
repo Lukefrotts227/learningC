8de33a7ad4ba44d35fa2ac27ece19ec4de007105
Ljf227 <ljfrotton@gmail.com>
2023-07-01 10:46:41 -0400

    updating in the car :)

--- cppbasics/studentmanage/manager.cpp
+++ cppbasics/studentmanage/manager.cpp
@@ -6,9 +6,15 @@
 
 using namespace std;
 bool keepgoing = true; 
-std::fstream file;
-class Student{
 
+class Student{
+    private: 
+        std::string name; 
+    public: 
+        string outputdata(){
+
+        return "";
+        }
 };
 
 class Course {
@@ -18,6 +24,7 @@ class Course {
         std::string teacherName; 
         int studentNumber; 
         int gradeNumber;
+        std::fstream file; 
         std::vector<Student> students;
 
     public: 
@@ -25,7 +32,7 @@ class Course {
         Course(int numS, int numG){
             studentNumber = numS; 
             gradeNumber = numG; 
-            file.open("class.csv", std::ios::app); 
+            file.open("metaclass.csv", std::ios::app); 
         }
 
         void addStudent(Student student){
@@ -33,6 +40,9 @@ class Course {
         }
         void updateCSV(){
             file.open("class.csv", std::ios::app);
+            for (int i = 0; i < students.size(); i++){
+                file << students[i].outputdata(); 
+            }
 
         }
         
@@ -42,12 +52,19 @@ class Course {
 
 // function to reset everything
 void reset(){
+    fstream file; 
+    fstream file2; 
+    file.open("class.csv", std::ios::); 
+    file2.open("metaclass.csv", std::ios::);
+
+    file.close(); 
+    file2.close();  
 
 }
 
 // the main function
 int main(void){
-
+    // main loop of the program; 
     while (keepgoing == true){
 
     }
