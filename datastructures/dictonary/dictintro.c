3403470c1a88dc9771021388d94f2a07788ec4fb
Ljf227 <ljfrotton@gmail.com>
2023-07-08 11:54:17 -0400

    data structs

--- datastructures/dictonary/dictintro.c
+++ datastructures/dictonary/dictintro.c
@@ -1,9 +1,16 @@
 #include <stdio.h>  
-typedef struct node{
-    char *key; 
-    char *value; 
-    
-}node; 
+typedef struct {
+    const char *key; 
+    int value;
+} entry; 
+
+typedef struct {
+    int len; 
+    int cap; 
+    entry *ent; 
+}ss, sst; 
+
+
 
 
 int main(void){
