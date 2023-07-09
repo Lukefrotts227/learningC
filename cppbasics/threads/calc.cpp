6ac2e4a3c9e78bc4416558f86c8dc7ae2cc0328d
Ljf227 <ljfrotton@gmail.com>
2023-07-08 22:36:07 -0400

    got it working to satisfy

--- cppbasics/threads/calc.cpp
+++ cppbasics/threads/calc.cpp
@@ -1,65 +1,56 @@
-#include <iostream> 
-#include <thread> 
-#include <vector> 
-
+#include <iostream>
+#include <thread>
+#include <vector>
 
 using namespace std;
 
-
 // return all of the factorial answers
-void print_all(vector<int>& ans)
+void print_all(const vector<int>& ans)
 {
-    cout << "The factorials in order are... \n"; 
-    for (int n : ans){
-        cout << n << " "; 
+    cout << "The factorials in order are...\n";
+    for (int n : ans) {
+        cout << n << " ";
     }
-    cout << "\n thats all!! \n"; 
-
+    cout << "\nThat's all!!\n";
 }
-
 
 // calculate the factorial of a given number using a standard recursive function
 int factorial(int number)
-{   
-    if (number == 0){
-        return 1; 
-    }
-    return number * factorial(number - 1); 
-
-}
-
-vector<int> get_port(int begin, int end, vector<int>& og){
-
-    vector<int> newvec; 
-
-    for (int i = begin; i < end; i++){
-        int answer = factorial(og[i]); 
-        newvec.push_back(answer); 
-    }
-
-    return newvec; 
-}
-
-
-
- 
-int main(int argc, char *argv[])
 {
-
-    vector<int> nums; 
-
-    for(int i = 1; i < argc; i++)
-    {
-        int num = stoi(argv[i]); 
-        nums.push_back(num); 
+    if (number == 0) {
+        return 1;
     }
-    int start1 = 1; 
-    int start2 = (argc/2) + 1; 
-    int end1 = start2; 
-    int end2 = argc; 
-    
-    vector<int> half1; 
-    vector<int> half2; 
+    return number * factorial(number - 1);
+}
+
+vector<int> get_port(int begin, int end, const vector<int>& og)
+{
+    vector<int> newvec;
+
+    for (int i = begin; i < end; i++) {
+        int answer = factorial(og[i]);
+        newvec.push_back(answer);
+    }
+
+    return newvec;
+}
+
+int main(int argc, char* argv[])
+{
+    vector<int> nums;
+
+    for (int i = 1; i < argc; i++) {
+        int num = stoi(argv[i]);
+        nums.push_back(num);
+    }
+
+    int start1 = 0; // Corrected: Start index of the first half
+    int start2 = nums.size() / 2; // Corrected: Start index of the second half
+    int end1 = start2; // Corrected: End index of the first half
+    int end2 = nums.size(); // Corrected: End index of the second half
+
+    vector<int> half1;
+    vector<int> half2;
 
     // Create threads for each half
     thread thread1(get_port, start1, end1, std::ref(nums));
@@ -81,6 +72,5 @@ int main(int argc, char *argv[])
     // Print the combined vector of factorials
     print_all(combined);
 
-
-    return 0; 
+    return 0;
 }
