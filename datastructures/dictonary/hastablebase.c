4bd2c23dcf9a6115c3390d19b6a4503ca7b0cb11
Ljf227 <ljfrotton@gmail.com>
2023-07-29 10:41:14 -0400

    more hashtable work

--- datastructures/dictonary/hastablebase.c
+++ datastructures/dictonary/hastablebase.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include <string.h> 
+#include <stdbool.h>
 
 #define MAX_SIZE 256 
-#define TABLE_SIZE 2000
+#define TABLE_SIZE 99
 
 
 typedef struct node{
@@ -11,25 +12,86 @@ typedef struct node{
     int data; 
 }node; 
 
-unsigned int has(char *name, int size){
+node * hash_table[TABLE_SIZE];
+
+unsigned int hash(const char *name, int size){
     int len = strnlen(name, MAX_SIZE); 
 
     unsigned int hashval = 0; 
     for (int i = 0; i < len; i++){
         hashval += name[i]; 
-        hashval = (hashval * name[i]) % TABLE_SIZE; 
+        hashval = (hashval * name[i]) % size; 
 
     }
 
     return hashval; 
 }
 
-int main(void){
-    char *names = {"bob", "henry", "joey", "tom", "julia", "jane"};
-    int name_len = 6; 
-    for (int i = 0; i < name_len; i++){
-        printf("%s => %u", names[i], hash(names[i]));
+bool init_table(){
+    for(int i = 0; i < TABLE_SIZE; i++){
+        hash_table[i] = NULL; 
     }
+    return true; 
+}
+
+void print_table(){
+    printf("Start:\n");
+    for(int i = 0; i < TABLE_SIZE; i++){
+        if (hash_table[i] == NULL){
+            printf("\t%i\t---\n",i);
+        }
+        else { 
+            printf("\t%i\t%s\n", i, hash_table[i]);
+        }
+    }
+    printf("Finish:\n");
+}
+
+bool insert_item(node *p){
+    if(p == NULL) return false; 
+    int index = hash(p->key, TABLE_SIZE); 
+    if (hash_table[index] != NULL){
+        return false;
+    }
+    hash_table[index] = p; 
+    return true; 
+}
+
+node * table_lookup(char *name){
+    int index = hash(name, TABLE_SIZE); 
+    if (hash_table[index] !=  NULL && strncmp(hash_table[index]->key, name, TABLE_SIZE) ==0){
+        return hash_table[index];
+
+    }
+    return NULL;
+}
+
+int main(void){
+    init_table(); 
+
+    print_table(); 
+    
+    node tom = {.key="tom", .data=22}; 
+    node jon = {.key="jon", .data=13};
+    node johan = {.key="johan", .data=99};
+    node earl = {.key="earl", .data=25};
+    node jane = {.key="jane", .data=12};
+
+
+    insert_item(&tom);
+    insert_item(&jon);
+    insert_item(&johan);
+    insert_item(&earl);
+    insert_item(&jane);
+
+    print_table(); 
+
+
+
+    /*for (int i = 0; i < name_len; i++){
+        printf("%s => %u\n", names[i], hash(names[i], TABLE_SIZE));
+    }
+    */
 
     return 0;
 }
