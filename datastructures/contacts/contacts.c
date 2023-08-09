a3c39635fe29c217d875f9dffb52597fdd89c026
Ljf227 <ljfrotton@gmail.com>
2023-08-09 11:14:26 -0400

    some file i/o bugs to iron out

--- datastructures/contacts/contacts.c
+++ datastructures/contacts/contacts.c
@@ -5,6 +5,9 @@
 #include <stdbool.h> 
 
 
+#define FILE_NAME "contacts.txt"
+#define OTHER_SIZE 8
+
 char * to_lowercase(char *str){
     for(int i = 0; str[i]; i++){
         str[i] = tolower(str[i]); 
@@ -12,9 +15,6 @@ char * to_lowercase(char *str){
     return str; 
 }
 
-int make_choice(char * str){
-    return 0; 
-}
 
 bool keeper(char * str){
     char * strt = to_lowercase(str); 
@@ -26,7 +26,11 @@ bool keeper(char * str){
 
 
 char * get_name(){
-    char * name; 
+    char *name = (char *)malloc(sizeof(char) * MAX_SIZE); 
+    if(name == NULL){
+        fprintf(stderr, "memory alloaction failure\n"); 
+        exit(0); 
+    }   
     printf("Enter the name: "); 
     scanf("%s", name); 
     
@@ -112,7 +116,7 @@ void load_to_file(table cont, unsigned int size, char * file_name){
         fprintf(file_p, "%s %lld\n",cont.extras[i]->name, cont.extras[i]->number); 
     }
 
-
+    fclose(file_p);
     table_destroy(cont, size); 
 
 }
@@ -121,37 +125,82 @@ void load_to_file(table cont, unsigned int size, char * file_name){
 int main (void){
 
     unsigned int size; 
+    int max_cache; 
     bool keepgoing = true; 
+    int choice; 
+    contact * store; 
+    char * name_store; 
+    int number_store; 
+    char * temp = (char* )malloc(sizeof(char) * OTHER_SIZE); 
 
     printf("Welcome to Lukas's contact manager\n"); 
 
     printf("Enter the intro size of the table: "); 
     scanf("%d", &size); 
 
-    table myContact = load_from_file(size, "contacts.txt"); 
+    printf("Enter the max size of the tables cache: "); 
+    scanf("%d", &max_cache); 
 
-
-    while(keepgoing == true){
-
-
-
-        printf("Do you wish to remain in the program: "); 
-        char * temp; 
-        scanf("%s", temp); 
-        keepgoing = keeper(temp); 
-    }
-
-
-
-
-
-
-
-   
+    table myContacts = load_from_file(size, FILE_NAME); 
 
     
+    while(keepgoing == true){
+
+        if(myContacts.cache_size > max_cache){
+            table_resize(myContacts, size, size+15); 
+            size = size + 15; 
+        }
+
+        printf("Choose which action you want to perform\n1. Do you want to find a person\n2. Do you want to add a new person\n3. Do you want to remove a person\n");
+        printf("Enter your choice: "); 
+        scanf("%d", &choice); 
+        getchar();
+
+        if(choice == 1){
+            name_store = get_name(); 
+
+            store = uni_lookup(myContacts, size, name_store); 
+
+            if(store == NULL){
+                printf("Name not found!!!\n");
+            }
+
+            else{
+                printf("The name is: %s\n", store->name); 
+                printf("The number is %lld\n", store->number); 
+            }
+            
+
+        }
+        else if(choice == 2){   
+            number_store = get_phonenumber(); 
+            getchar(); 
+            name_store = get_name(); 
+
+            myContacts = table_insert(myContacts, size, name_store, number_store); 
+
+        }
+        else if(choice == 3){
+            name_store = get_name(); 
+
+            myContacts = table_delete(myContacts, size, name_store); 
+
+        }
+        else{
+            printf("Invalid choice!!!\n");
+        }
+         
+        printf("Do you wish to remain in the program: "); 
+        
+        scanf("%s", temp); 
+        keepgoing = keeper(temp); 
 
 
+    }
+    free(temp); 
+    free(name_store); 
+    printf("Okay great!!\n"); 
+    load_to_file(myContacts, size, FILE_NAME); 
 
 
 }
