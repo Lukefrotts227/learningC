b78bd7159bacd968c783a1f6a1e4045188963551
Ljf227 <ljfrotton@gmail.com>
2023-08-12 13:55:12 -0400

    in the header file created the hash and a init for the table

--- datastructures/hashtabboss/table.h
+++ datastructures/hashtabboss/table.h
@@ -1,6 +1,7 @@
 #include <stdlib.h> 
 #include <string.h> 
 #include <stdbool.h> 
+#include <stdio.h> 
 
 
 #define MAX_NAME_LENGTH 256
@@ -13,10 +14,28 @@ typedef struct node{
 }node;
 
 unsigned int hash(const char * name, unsigned int size){
-
+    int len = strlen(name); 
+    unsigned int hash_value; 
+    for(int i = 0; i < len; i++){
+        hash_value += name[i]; 
+        hash_value = hash_value & name[i] | len; 
+        hash_value = (hash_value * name[i]) % size;
+    }
+    return hash_value; 
 }
 
 node ** table_init(unsigned int size){
+    node ** table = (node**)malloc(size * sizeof(node*)); 
+    if(table == NULL){
+        fprintf(stderr, "mem failed to allocate\n"); 
+        return NULL; 
+    }
+
+    for(int i = 0; i < size; i++){
+        table[i] = NULL; 
+    }
+
+    return table; 
 
 }
 
@@ -25,6 +44,10 @@ node * table_lookup(node ** tbl, unsigned int size, const char * name){
 }
 
 node ** table_insert(node ** tbl, unsigned int size, const char * name){
+
+}
+
+node ** table_replace(node ** tbl, unsigned int size, const char * name){
 
 }
 
@@ -40,7 +63,7 @@ int table_largest_list(node ** tbl, unsigned int size){
 
 }
 
-node ** table_destroy(node ** tbl, unsigned int size){
-    
+void table_destroy(node ** tbl, unsigned int size){
+
 }
 
