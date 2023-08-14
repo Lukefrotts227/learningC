c5db560db45390ff84cc51b387200eba02109176
Ljf227 <ljfrotton@gmail.com>
2023-08-13 23:14:33 -0400

    more of it btw

--- datastructures/hashtablecpp/table.cpp
+++ datastructures/hashtablecpp/table.cpp
@@ -9,21 +9,50 @@ using namespace std;
 
 
 
-template <typename T, typename H>
+template <typename Key, typename Data>
 class Node{
     public: 
-        H key;
-        T data; 
+        Key key;
+        Data data; 
         Node * next; 
 
-        Node(const H& n, const T& val) : key(n), data(val), next(nullptr)
+        Node(const Key& n, const Data& val) : key(n), data(val), next(nullptr)
 
 }; 
 
-template <typename T, typename H>
+template <typename Key, typename Data>
 class Table{
     private: 
+        unsigned int size; 
+        Node<Key, Data> * table; 
+        unsigned int (*hash)(Key key); 
+
         
     public: 
+        Table(unsigned int s, unsigned int(*h)(Key)) : size(s) hash(h){
+            table = new Node<Key, Data>[size]; 
+        }
 
+        ~Table() {
+        delete[] table; 
+        }
+        unsigned int get_index(const Key& key){
+            return hash(key) % size; 
+        }
+
+        void insert(const Key& key, const Data& data){
+            unsigned int ind =  get_index(key); 
+
+            Node<Key, Data>* newNode = new Node<Key, Data>(key, data); 
+
+            newNode->next = table[ind].next;
+            table[ind].next = newNode; 
+
+        }
 }; 
+
+
+int main(){
+
+    return 0; 
+}
