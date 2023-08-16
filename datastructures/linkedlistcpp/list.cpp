9ead8974750dadcd36def134d49b250ec27e7a81
Ljf227 <ljfrotton@gmail.com>
2023-08-16 17:12:50 -0400

    filling out more of the doubly linked list

--- datastructures/linkedlistcpp/list.cpp
+++ datastructures/linkedlistcpp/list.cpp
@@ -16,9 +16,45 @@ class Node{
 template <typename Data> 
 class DoublyLinkedList{
     private: 
-
+        Node<Data> * head; 
+        Node<Data> * tail; 
+        unsigned int size; 
 
     public: 
+        DoublyLinkedList() : head(nullptr), tail(nullptr), size(0) {}
+
+
+        bool append(const Data& data){
+            Node<Data> * newNode = new Node<Data>(data); 
+        }
+
+        bool prepend(const Data& data){
+            Node<Data> * newNode = new Node<Data>(data); 
+
+        }
+
+        bool insert(const Data& data, unsigned int pos){
+
+        }
+
+        bool del(const Data& data){
+
+        }
+
+        bool del_pos(unsigned int pos){
+            
+        }
+
+
+        ~DoublyLinkedList(){
+            Node<Data>* current = head; 
+
+            while(current){
+                Node<Data>* next = current->next;
+                delete current; 
+                current = next;  
+            }
+        }
 }; 
 
 
