88df6c37c7185063db94ac3a2650ca29c87b7d5d
Ljf227 <ljfrotton@gmail.com>
2023-08-16 10:42:55 -0400

    with this cpp hashtable I can store my own defined data types as shown in the shopping list

---
+++ datastructures/hashtablecpp/shopping.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+#include "table.h"
+
+class Entry{
+    public: 
+        double cost; 
+        int amount; 
+
+        Entry(double c, int a) : cost(c), amount(a){}
+
+};
+
+
+int main(){
+
+}
