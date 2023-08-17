c579af82d520bd211c94365581963cc959f5788c
Ljf227 <ljfrotton@gmail.com>
2023-08-17 11:38:12 -0400

    filling out more of the doubly linked list

--- datastructures/linkedlistcpp/list.cpp
+++ datastructures/linkedlistcpp/list.cpp
@@ -3,13 +3,16 @@
 #include <string> 
 
 
+using namespace std; 
+
 template <typename Data> 
 class Node{
-    Data data; 
-    Node * prev; 
-    Node * next; 
+    public:
+        Data data; 
+        Node * prev; 
+        Node * next; 
 
-    Node(const Data& d) : Data(d), prev(nullptr) next(nullptr) {}
+        Node(const Data& d) : data(d), prev(nullptr), next(nullptr) {}
 };
 
 
@@ -22,31 +25,157 @@ class DoublyLinkedList{
 
     public: 
         DoublyLinkedList() : head(nullptr), tail(nullptr), size(0) {}
-
-
-        bool append(const Data& data){
-            Node<Data> * newNode = new Node<Data>(data); 
-        }
-
+        
         bool isEmpty(){
             return size == 0; 
         }
 
+
+        bool append(const Data& data){
+            Node<Data> * newNode = new Node<Data>(data); 
+            if(this->isEmpty()){
+                head = newNode; 
+                tail = newNode; 
+                size++; 
+                return true;
+            }
+            tail->next = newNode; 
+            newNode->prev = tail; 
+            tail = newNode; 
+            size++;
+            return true; 
+
+
+        }
+
+
         bool prepend(const Data& data){
             Node<Data> * newNode = new Node<Data>(data); 
-
+            if(this->isEmpty()){
+                head = newNode; 
+                tail = newNode; 
+                size++; 
+                return true; 
+            }
+            head->prev = newNode; 
+            newNode->next = head; 
+            head = newNode; 
+            size++; 
+            return true; 
         }
 
         bool insert(const Data& data, unsigned int pos){
+            if(pos >=size || pos < 0){
+                return false; 
+            }
 
+            if(pos == 0){
+                this->prepend(data); 
+                return true; 
+            } 
+
+            if(pos == size){
+                this->append(data); 
+                return true; 
+            }
+            Node<Data>* newNode = new Node<Data>(data);
+            Node(Data)* current; 
+            
+
+            if(pos > size/2){
+                current = tail; 
+                for(int i = size; i > pos; i--){    
+                    current = current->prev; 
+                }
+
+            }else{
+                current = head; 
+                for(int i = 0; i < pos; i++){
+                    current = current->next; 
+                }
+            }
+
+            newNode->next = current; 
+            newNode->prev = current->prev; 
+            current->prev->next = newNode; 
+            current->prev = newNode; 
+
+            return true;
+
+        }
+
+        Node<Data> * search(const Data& data, bool fromFront){
+            Node<Data> * current; 
+            if(fromFront){
+                current = head; 
+                while(current != nullptr){
+                    if(current-data == data){
+                        return current; 
+                    }
+                }
+            }else{
+                current = tail
+                while(current != null ptr){
+                    if(current->data == data){
+                        return current; 
+                    }
+                    current = current->prev; 
+                }
+            }
+
+            return nullptr; 
+
+        }
+        Node<Data> * search_post(int pos){
+            if(pos >= size || pos < 0){
+                return nullptr; 
+            }
+            Node<Data> * current; 
+
+            if(pos > size/2){
+                current = tail; 
+                for(int i = size; i > pos; i--){
+                    current = current->prev; 
+                }
+            }else{
+                current = head; 
+                for(int i = 0; i < pos; i++){
+                    current = current->next; 
+                }
+
+            }
+
+            return current; 
         }
 
         bool del(const Data& data){
-
+            return true; 
         }
 
         bool del_pos(unsigned int pos){
+            if(pos >= size || pos < 0){
+                return false;
+            }
+            return true; 
+        }
 
+        void print(bool fromFront){
+            if(fromFront){
+                Node<Data> * current = head; 
+
+                while(current != nullptr){
+                    cout << current->data << "--"; 
+                    current = current->next; 
+                }
+            }else{
+                Node<Data> * current = tail; 
+
+                while(current != nullptr){
+                    cout << current->data << "--"; 
+                    current = current->prev; 
+                }
+            }
+            cout << '\n'; 
         }
 
 
@@ -64,6 +193,14 @@ class DoublyLinkedList{
 
 
 int main(){
+    DoublyLinkedList<string> mylist; 
+
+    mylist.append("billy"); 
+    mylist.append("john"); 
+    mylist.prepend("jose"); 
+
+    mylist.print(true); 
+    mylist.print(false); 
 
     return 0; 
 }
