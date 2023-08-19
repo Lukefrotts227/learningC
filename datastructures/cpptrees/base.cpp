33f22d72cf6e713ca2efb572462cb48778a47af4
Ljf227 <ljfrotton@gmail.com>
2023-08-19 13:47:39 -0400

    doing my bin search tree

--- datastructures/cpptrees/base.cpp
+++ datastructures/cpptrees/base.cpp
@@ -8,8 +8,8 @@ template <typename Data>
 class Node{
     public: 
         Data data;  
-        Node * child; 
-        Node * partner; 
+        Node * left; 
+        Node * right; 
 
         Node(const Data& value) : data (value), left(nullptr), right(nullptr){}
 
@@ -21,14 +21,28 @@ class Tree {
     private: 
         Node<Data> * root; 
 
+        Node<Data> * insert(Node<Data> * node, const Data& data){
+            if(node == nullptr){
+                return new Node<Data>(data); 
+            }
+
+            if(data < node->data){
+                node->left = insert(node->left, data); 
+            }else if(data > node-> data){
+                node->right = insert(node->right, data); 
+            }
+
+            return node; 
+        }
+
     public: 
         Tree() : root(nullptr){}
 
-        
 
-        bool insert(const Data& data, const Data& parent){
+
+        bool insert(const Data& data){
             
-
+            root = insert(root, data); 
         
         }
 
