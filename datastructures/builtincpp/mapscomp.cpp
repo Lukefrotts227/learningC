4f8f763841ffa6d62fb18d250380d3c5e097978b
Ljf227 <ljfrotton@gmail.com>
2023-08-20 14:41:45 -0400

    my mapscomp:

--- datastructures/builtincpp/mapscomp.cpp
+++ datastructures/builtincpp/mapscomp.cpp
@@ -6,6 +6,7 @@
 #include <unordered_map> 
 #include <map> 
 #include "standards.h"
+#include <chrono>
 
 
 
@@ -26,8 +27,53 @@ vector<int> getValues(){
 int main(void){
     vector<string> namesArray = getKeys(); 
     vector<int> numbersArray = getValues(); 
-
     
+
+    map<string, int> myMap; 
+    auto start = chrono::high_resolution_clock::now(); 
+    for(size_t i = 0; i < namesArray.size(); ++i){
+        myMap[namesArray[i]] = numbersArray[i]; 
+    }
+    auto end = chrono::high_resolution_clock::now(); 
+    chrono::duration<double> duration = end - start; 
+
+    cout << "map insert " << duration.count() << " seconds\n"; 
+
+    start = chrono::high_resolution_clock::now(); 
+
+    for(const auto& name : namesArray){
+        int value = myMap[name]; 
+    }
+    end = chrono::high_resolution_clock::now(); 
+
+    duration = end - start; 
+    cout << "map access time: " << duration.count() << "seconds\n"; 
+
+    unordered_map<string, int> myUnorderedMap; 
+    start = chrono::high_resolution_clock::now(); 
+
+    for(size_t i = 0; i < namesArray.size(); ++i){
+        myUnorderedMap[namesArray[i]] = numbersArray[i]; 
+    }
+
+    end = chrono::high_resolution_clock::now(); 
+
+    duration = end - start; 
+    cout << "unordered map insert time: " << duration.count() << "seconds\n"; 
+
+    start = chrono::high_resolution_clock::now(); 
+    for(const auto& name : namesArray){
+        int value = myUnorderedMap[name]; 
+    }
+
+    end = chrono::high_resolution_clock::now(); 
+    duration = end - start; 
+
+    cout << "unorderd map  access time: " << duration.count() << "seconds\n"; 
+    
+
+
+
 
 
 
