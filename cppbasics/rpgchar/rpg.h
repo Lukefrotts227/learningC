ae6371e3b3d1a8db6a9be3d8b10301cb7fc85666
Ljf227 <ljfrotton@gmail.com>
2023-08-24 18:01:55 -0400

    the rpg project is great teacher of oop

--- cppbasics/rpgchar/rpg.h
+++ cppbasics/rpgchar/rpg.h
@@ -1,70 +1,87 @@
-#include <iostream> 
-#include <vector> 
-#include <string> 
-#include <map> 
-
+#include <iostream>
+#include <map>
+#include <string>
 
 #define MAX_STATS 100.0
 #define MIN_STATS 0.0
 
-using namespace std; 
 
-class stats {
-private:
-    map<std::string, double> statistics;
 
-public:
-    stats() {}
 
-    bool statCheck(double stat) {
-        return (stat <= MAX_STATS) && (stat >= MIN_STATS);
-    }
+std::map<std::string, std::string> keywords; 
 
-    void setStat(const string& name, double value) {
-        if (statCheck(value)) {
-            statistics[name] = value;
+
+class AttributeContainer {
+    private:
+        std::map<std::string, double> attributes;
+
+    protected:
+        bool checkAttribute(double value) {
+            return (value <= MAX_STATS) && (value >= MIN_STATS);
         }
-    }
 
-    double getStat(const string& name) const {
-        auto it = statistics.find(name);
-        if (it != statistics.end()) {
-            return it->second;
+    public:
+        AttributeContainer() {}
+
+        void setAttribute(const std::string& name, double value) {
+            if (checkAttribute(value)) {
+                attributes[name] = value;
+            }
         }
-        return 0.0;
-    }
+
+        double getAttribute(const std::string& name) const {
+            auto it = attributes.find(name);
+            if (it != attributes.end()) {
+                return it->second;
+            }
+            return -1.0;
+        }
 };
 
+class stats : public AttributeContainer {
+    public:
+        stats() {}
+};
 
-class skills{
-    private: 
+class skills : public AttributeContainer {
+    public:
+        skills() {}
+};
 
+class specials : public AttributeContainer {
+    public:
+        specials() {}
+};
+
+class weapon : public AttributeContainer{
     public: 
+        weapon() {}
 }; 
 
-
-class specials{
-    private: 
+class clothing : public AttributeContainer{
 
     public: 
+        clothing() {}
 }; 
 
-class weapon{
-    private: 
-
+class item : public AttributeContainer{
     public: 
-
-
+        item() {}
 }; 
 
-class clothing{
-    private: 
-
+class morality : public AttributeContainer {
     public: 
+        morality() {}
 }; 
 
-class item{
+class character{
     private: 
+        stats stat; 
+        skills skill; 
+        specials special; 
+        morality moral;  
 
     public: 
+        character(){}
+
 }; 
