7f7b6251eef75d5f7b450fb23ebe3f2ba9b10514
Ljf227 <ljfrotton@gmail.com>
2023-09-05 14:10:37 -0400

    practicing iterator

---
+++ cppbasics/iterators/mine.cpp
@@ -0,0 +1,45 @@
+#include <iostream> 
+#include <vector> 
+
+using namespace std; 
+
+
+class myClass{
+
+    private: 
+        vector<int> myclass; 
+
+    public: 
+
+        
+        
+        void push_back(int item){
+            myclass.push_back(item);
+             
+        }
+
+        int getSize(){
+            return myclass.size(); 
+        }
+
+
+
+
+        class Iterator{
+            private: 
+
+
+            public: 
+            
+        }
+
+
+
+}
+
+
+
+int main(){
+
+    return 0; 
+}
